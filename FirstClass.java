03ac03d96508db461f97ef36f9a7dd352af329f3
BRV12G <159134024+BRV12G@users.noreply.github.com>
2024-08-04 01:48:29 +0530

    first basics in java

---
+++ FirstClass.java
@@ -0,0 +1,37 @@
+//SCANNER CLASS
+
+import java.util.Scanner;
+
+public class FirstClass{
+    public static void main(String args[])
+    {
+        // System.out.println("hello");
+        // System.out.println(1234);
+
+        // Scanner s = new Scanner(System.in);
+        // System.out.println("enter here");
+        // System.out.println(s.nextInt());
+        //System.out.println(s.next());
+        // System.out.println(s.nextLine());
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("ENter your RollNumber");
+        // int rollno = input.nextInt();
+        // System.out.println("The roll Number is : " + rollno);
+        // String name = input.nextLine();
+        // System.out.println(name);
+        // float marks = input.nextFloat();
+        // System.out.println(marks);
+        boolean check = input.nextBoolean();
+        System.out.println(check);
+
+
+
+
+        // int a = 230_000_000;
+        // System.out.println(a);
+
+
+    }
+}
+
