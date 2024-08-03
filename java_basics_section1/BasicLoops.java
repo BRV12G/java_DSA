cf44525bdbac838ba594b45266ffca898d77b724
BRV12G <159134024+BRV12G@users.noreply.github.com>
2024-08-04 03:23:20 +0530

    basic if for while learned

---
+++ java_basics_section1/BasicLoops.java
@@ -0,0 +1,27 @@
+package java_basics_section1;
+
+public class BasicLoops {
+    
+    public static void main(String[] args) {
+        //if 
+        int a = 20;
+        if (a==20){
+            System.out.println("hello " + a);
+        }
+
+        //while 
+        int count = 1;
+        while(count!=5){
+            System.out.println(count);
+            count++;
+        }
+
+        //for 
+        for(int count2 = 1 ; count2 != 5 ; count2++){
+            System.out.println(count2);
+
+        }
+        
+    }
+    
+}
