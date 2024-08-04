4922465a97f9867195f167573985aae8ea49cb95
BRV12G <159134024+BRV12G@users.noreply.github.com>
2024-08-05 00:41:54 +0530

    if and else if

---
+++ java_basics_section1/ConditionalsInJava.java
@@ -0,0 +1,37 @@
+package java_basics_section1;
+
+public class ConditionalsInJava {
+    
+    // IF ELSE
+    // public static void main(String[] args) {
+    //     int salary = 2500000;
+    //     if(salary>10000)
+    //     {
+    //         salary = salary + 20000;
+
+    //     }
+    //     else{
+    //         salary = salary +10000;
+    //     }
+
+    //     System.out.println(salary);
+    // }
+
+    //MULTIPLE IF ELSE
+    // public static void main(String[] args) {
+    //         int salary = 2500;
+    //         if(salary>10000)
+    //         {
+    //             salary = salary + 20000;
+    
+    //         }
+    //         else if(salary>5000){
+    //             salary = salary +30000;
+    //         }
+    //         else{
+    //             salary = salary +10000;
+    //         }
+    
+    //         System.out.println(salary);
+    //      }
+}
