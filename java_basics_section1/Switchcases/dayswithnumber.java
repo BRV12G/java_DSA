27774fa384d9d30d002bd13ba128659bef6c8904
BRV12G <159134024+BRV12G@users.noreply.github.com>
2024-08-09 16:05:47 +0530

    switch basic example

---
+++ java_basics_section1/Switchcases/dayswithnumber.java
@@ -0,0 +1,5 @@
+package java_basics_section1.Switchcases;
+
+public class dayswithnumber {
+    
+}
---
+++ java_basics_section1/Switchcases/switchCases.java
@@ -0,0 +1,52 @@
+package java_basics_section1.Switchcases;
+
+import java.util.Scanner;
+
+public class switchCases {
+   //1. Normal switch example
+
+    // public static void main(String[] args) {
+    //     Scanner input = new Scanner(System.in);
+
+    //     String fruit = input.next();
+
+    //     switch(fruit){
+    //         case "Mango" :
+    //         System.out.println("king of fruits");
+    //         break;
+
+    //         case "apple" :
+    //         System.out.println("all seasons");
+    //         break;
+
+    //         case "stawberry" :
+    //         System.out.println("seasonal");
+    //         break;
+
+    //         default:
+    //         System.out.println("invalid fruit");
+
+            
+
+    //     }
+
+    //     input.close();
+    // }
+
+    //2. enhanced switch example
+    // public static void main(String[] args) {
+    //     Scanner input = new Scanner(System.in);
+
+    //      String fruit = input.next();
+    //      switch(fruit){
+    //         case "Mango" -> System.out.println("king");
+    //         case "apple" -> System.out.println("Seasonal");
+    //         default -> System.out.println("invalid fruit");
+    //      }
+
+
+    //      input.close();
+    // }
+
+    
+}
