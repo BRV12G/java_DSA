02cadee13e2b28e960afa8aada79043ff1790381
BRV12G <159134024+BRV12G@users.noreply.github.com>
2024-08-10 23:27:35 +0530

    VOLUME OF CONE

---
+++ java_basics_section1/Switchcases/ASSignment2/Volume.java
@@ -0,0 +1,21 @@
+package java_basics_section1.Switchcases.ASSignment2;
+
+import java.util.Scanner;
+
+public class Volume {
+
+    //14.VOLUME OF CONE 
+    // public static void main(String[] args) {
+    //     Scanner in = new Scanner(System.in);
+    //     System.out.println("Enter the radius");
+    //     double radius = in.nextDouble();
+    //     System.out.println("Enter the height");
+    //     double height = in.nextDouble();
+    //     double volume = (1.0/3.0)*Math.PI*(Math.pow(radius,2))*height;
+    //     System.out.println("volume is: " +volume);
+
+    // }
+     
+    
+    
+}
