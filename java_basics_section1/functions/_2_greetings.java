3478cdf5692e3f78f6ebc90029b780610178f8ff
BRV12G <159134024+BRV12G@users.noreply.github.com>
2024-08-18 01:12:17 +0530

    void return type

---
+++ java_basics_section1/functions/_2_greetings.java
@@ -0,0 +1,10 @@
+package java_basics_section1.functions;
+
+public class _2_greetings {
+    public static void main(String[] args) {
+        greetings();
+    }
+   static void greetings(){
+        System.out.println("greetings of");
+    }
+}
